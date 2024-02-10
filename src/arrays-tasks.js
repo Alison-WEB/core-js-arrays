b0975723a320b7f63192dae52902daf7c0e3d081
Alison WEB <AlisonWEB@yandex.ru>
2024-02-10 20:53:11 +0300

    feat: pass sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -41,8 +41,14 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  let newArr;
+  if (arr1.length >= arr2.length) {
+    newArr = arr1.map((value, index) => value + (arr2[index] || 0));
+  } else {
+    newArr = arr2.map((value, index) => value + (arr1[index] || 0));
+  }
+  return newArr;
 }
 
 /**

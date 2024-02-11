e7a56d950a5731e365910482b46b579de7d992b2
Alison WEB <AlisonWEB@yandex.ru>
2024-02-11 04:30:55 +0300

    feat: pass getFalsyValuesCount

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -418,8 +418,9 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const sum = arr.filter((elem) => !elem);
+  return sum.length;
 }
 
 /**

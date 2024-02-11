03aba2ad5edd69eb9854d887b396d0dba3146589
Alison WEB <AlisonWEB@yandex.ru>
2024-02-11 04:05:08 +0300

    feat: pass getElementByIndices

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -397,8 +397,13 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  if (indices.length === 0) {
+    return arr;
+  }
+  const index = indices[0];
+  const restIndices = indices.slice(1);
+  return getElementByIndices(arr[index], restIndices);
 }
 
 /**

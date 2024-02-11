5c5df53ecedac03edc5167d1b5764d3518c592b6
Alison WEB <AlisonWEB@yandex.ru>
2024-02-11 03:34:21 +0300

    feat: pass generateOdd

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -378,8 +378,11 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const arr = [];
+  arr.length = len;
+  const newArr = Array.from(arr, (value, index) => index * 2 + 1);
+  return newArr;
 }
 
 /**
